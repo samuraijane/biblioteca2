a62bee8b74c11a932727b84cc9573c90d583f92a
Matthew Day <matt@matthewday.net>
2020-10-22 19:09:42 -0400

    Complete step 02-02

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/heartbeat', (req, res) => {
+  res.json({
+    is: "working"
+  })
+});
+
+
+app.listen(3001, () => {
+  console.log('The server is running at PORT 3001');
+});
