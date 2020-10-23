e31f9a6f8f09843c0202a614b11f7c7c0d87c391
Matthew Day <matt@matthewday.net>
2020-10-22 20:07:43 -0400

    Complete step 3

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.use("/css", express.static(__dirname + "/css"));
+app.use("/js", express.static(__dirname + "/js"));
+app.use("/", express.static(__dirname + "/public"));
+
 app.get('/heartbeat', (req, res) => {
   res.json({
     is: "working"
